4adc09afc383ebfd836207f7c4dae1461c139b67
eternalUnion <dnzbyk12345@gmail.com>
2023-08-02 15:46:56 +0300

    Updated gitignore, removed build files, added stripped assembly for relative reference

--- obj/Debug/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CrashRefrences
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CrashRefrences";
-        public const string PLUGIN_NAME = "CrashRefrences";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
